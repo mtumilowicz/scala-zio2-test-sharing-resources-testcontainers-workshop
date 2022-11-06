d96b3933efd0ad9fd48cb52d0a357019b8767615
mtumilowicz <michal.tumilowicz01@gmail.com>
2022-11-06 20:55:23 +0100

    fixing script

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sbt ++"$TRAVIS_SCALA_VERSION" publishAll
+sbt ++"$TRAVIS_SCALA_VERSION" publishLocalAll
 sbt ++"$TRAVIS_SCALA_VERSION" test
 docker-compose up -d && sbt ++"$TRAVIS_SCALA_VERSION" it:test
